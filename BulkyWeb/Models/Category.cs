b27bff3a00b8e2b82af9f9dc2d5db5f3f367b2df
Niraj Shrestha <niraj@crossovernepal.com>
2024-06-05 18:06:20 +0545

    Add project files.

---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+    public class Category
+    {
+
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? DispalyOrder { get; set; }
+
+    }
+}
