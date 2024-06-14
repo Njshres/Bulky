564308c505ed07e1efa04d98277b0d42f122616b
Niraj Shrestha <niraj@crossovernepal.com>
2024-06-14 17:02:45 +0545

    select list item problem solved

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -24,7 +25,11 @@ namespace Bulky.Models
         [Required]
         [Display(Name = " Price for 100+")]
         public double Price100 { get; set; }
+        [ValidateNever]
         public Category Category { get; set; }
+        
         public int CategoryId { get; set; }
+        [ValidateNever]
+        public string ImageUrl { get; set; }
     }
 }
--- BulkyWeb/Areas/Admin/Controllers/ProductsController.cs
+++ BulkyWeb/Areas/Admin/Controllers/ProductsController.cs
@@ -1,28 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Bulky.DataAccess.Data;
-using Bulky.Models;
-using Bulky.DataAccess.Repository.IRepository;
+//using BulkyWeb.ViewModel;
+//using System.Web.Mvc;
 
 namespace BulkyWeb.Areas.Admin.Controllers
 {
     public class ProductsController : Controller
     {
         private readonly IProductRepository _productRepo;
-
-        public ProductsController(IProductRepository context)
+        private readonly ICategoryRepository _categoryRepo;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductsController(IProductRepository context, ICategoryRepository db, IWebHostEnvironment webHostEnvironment)
         {
             _productRepo = context;
+            _categoryRepo = db;
+            _webHostEnvironment = webHostEnvironment;
         }
+      
 
+        //public ProductsController(IProductRepository context)
+        //{
+        //    _productRepo = context;
+        //}
         // GET: Products
         public IActionResult Index()
         {
+           
             return View(_productRepo.GetAll().ToList());
         }
 
@@ -45,82 +51,87 @@ namespace BulkyWeb.Areas.Admin.Controllers
         }
 
         // GET: Products/Create
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            //var model = new ProductVM
+            //{
+            //    CategoryList = _categoryRepo.GetAll().Select(c => new SelectListItem
+            //    {
+            //        Text = c.Name,
+            //        Value = c.Id.ToString()
+
+            //    }).ToList(),
+
+            //};
+
+            ProductVM productVM = new()
+            {
+                CategoryList = _categoryRepo.GetAll()
+               .Select(u => new SelectListItem
+               {
+                   Text = u.Name,
+                   Value = u.Id.ToString(),
+               }),
+                Product = new Product()
+            };
+            if (id == null || id == 0)
+            {
+                //create
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _productRepo.Get(u => u.Id == id);
+                return View(productVM);
+            }
         }
 
-        // POST: Products/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Create([Bind("Id,Title,Description,ISBN,ListPrice,Price,Price50,Price100")] Product product)
-        {
+                // POST: Products/Create
+                 // To protect from overposting attacks, enable the specific properties you want to bind to.
+                // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+         [HttpPost]
+         [ValidateAntiForgeryToken]
+         public IActionResult Upsert(ProductVM productVM, IFormFile? file)
+            {
             if (ModelState.IsValid)
             {
-                _productRepo.Add(product);
+                    if (productVM.Product.Id == 0)
+                    {
+                        _productRepo.Add(productVM.Product);
+                    }
+                    else
+                    {
+                       _productRepo.Update(productVM.Product);
+                    }
+
+                    string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath =Path.Combine(wwwRootPath, @"images\product");
+                    //string finalPath = Path.Combine(wwwRootPath, productPath);
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+                    productVM.Product.ImageUrl = @"images\product" + fileName;
+
+                }
+
+                _productRepo.Add(productVM.Product);
                 _productRepo.Save();
                 TempData["success"] = "Product added successfully";
                 return RedirectToAction(nameof(Index));
             }
-            return View(product);
-        }
+            return View();
+         }
 
-        // GET: Products/Edit/5
-        public IActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+         // GET: Products/Edit/5
+        
 
-            var product = _productRepo.Get(u => u.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return View(product);
-        }
-
-        // POST: Products/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,ISBN,ListPrice,Price,Price50,Price100")] Product product)
-        {
-            if (id != product.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-
-
-                _productRepo.Update(product);
-                _productRepo.Save();
-
-                //catch (DbUpdateConcurrencyException)
-                //{
-                //    if (!CategoryExists(category.Id))
-                //    {
-                //        return NotFound();
-                //    }
-                //    else
-                //    {
-                //        throw;
-                //    }
-                //}
-                TempData["success"] = "Product updated successfully";
-                return RedirectToAction(nameof(Index));
-            }
-            return View(product);
-        }
-
-        // GET: Products/Delete/5
-        public IActionResult Delete(int? id)
+         // GET: Products/Delete/5
+          public IActionResult Delete(int? id)
         {
             if (id == null)
             {
