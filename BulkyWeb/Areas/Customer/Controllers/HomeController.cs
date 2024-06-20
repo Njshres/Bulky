890a11161572f5180a6554e5861a4bd12f4d00ea
Niraj Shrestha <niraj@crossovernepal.com>
2024-06-20 16:56:12 +0545

    done upto home dashboard

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -6,18 +7,25 @@ namespace BulkyWeb.Areas.Customer.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IProductRepository _productRepo;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductRepository productRepo)
         {
             _logger = logger;
+            _productRepo = productRepo;
         }
 
         public IActionResult Index()
         {
-            return View();
+            List<Product> objProductList = _productRepo.GetAll(includeproperties: "Category").ToList();
+            return View(objProductList);
         }
-
+        public IActionResult Details(int productId)
+        {
+            Product product = _productRepo.Get( u=> u.Id==productId, includeproperties: "Category");
+            return View(product);
+        }
         public IActionResult Privacy()
         {
             return View();
